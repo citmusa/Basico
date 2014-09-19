54e2de9789d43fbbb7f9ffa9180b178a6df6639c
Citlalli Murillo <citmusa@hotmail.com>
2014-09-19 00:38:58 -0500

    initial commit

---
+++ app/src/main/java/app/citmusa/net/basico/NameDetailFragment.java
@@ -0,0 +1,36 @@
+package app.citmusa.net.basico;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+/**
+ * Created by citmusa on 9/19/14.
+ */
+public class NameDetailFragment extends Fragment {
+
+    private TextView txt_name;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_name_detail, null);
+        txt_name = (TextView)view.findViewById(R.id.text_name);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        Bundle arguments = getArguments();
+        String name = (String)arguments.get(MainActivity.NAME_TAG);
+        if (name != null){
+            txt_name.setText(name);
+        }
+
+        super.onActivityCreated(savedInstanceState);
+    }
+}
