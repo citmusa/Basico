06330a1978dd1c3473edf59667a7eb2e27974067
Citlalli Murillo <citmusa@hotmail.com>
2014-09-20 00:25:03 -0500

    Textview text-color in list changed

--- app/src/main/java/app/citmusa/net/basico/MainActivity.java
+++ app/src/main/java/app/citmusa/net/basico/MainActivity.java
@@ -1,14 +1,17 @@
 package app.citmusa.net.basico;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -30,7 +33,20 @@ public class MainActivity extends Activity {
         //Adaptador para la lista
         final ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(),
                                                                 android.R.layout.simple_list_item_1,
-                                                                names);
+                                                                names){
+            // this is dirty
+            // check http://stackoverflow.com/questions/4533440/android-listview-text-color
+            @Override
+            public View getView(int position, View convertView,
+                                ViewGroup parent) {
+                View view =super.getView(position, convertView, parent);
+
+                TextView textView=(TextView) view.findViewById(android.R.id.text1);
+                /*YOUR CHOICE OF COLOR*/
+                textView.setTextColor(Color.BLUE);
+                return view;
+            }
+        };
         list.setAdapter(adapter);
         btn_submit.setOnClickListener(new View.OnClickListener() {
             @Override
