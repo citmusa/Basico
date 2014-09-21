1ec47c0dd1918515e786e11ba93982472e944ecb
Citlalli Murillo <citmusa@hotmail.com>
2014-09-20 23:48:18 -0500

    Back in blank

--- app/src/main/java/app/citmusa/net/basico/MainActivity.java
+++ app/src/main/java/app/citmusa/net/basico/MainActivity.java
@@ -1,54 +1,18 @@
 package app.citmusa.net.basico;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
-
-import java.util.ArrayList;
 
 
 public class MainActivity extends Activity{
 
-    public static final String NAME_TAG = "name";
-    private static final ArrayList<String> names = new ArrayList<String>();
-    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        final EditText input_name = (EditText)findViewById(R.id.edit_name);
-        ListView list = (ListView)findViewById(R.id.list_names);
-        Button btn_submit = (Button)findViewById(R.id.button_submit);
-
-        //Adaptador para la lista
-        adapter = new ArrayAdapter<String>(getApplicationContext(),
-                                                                android.R.layout.simple_list_item_1,
-                                                                names);
-        list.setAdapter(adapter);
-        list.setOnItemClickListener( new MyItemClickListener() );
-
-        btn_submit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String name = input_name.getText().toString().trim();
-                if (!names.contains(name) && !name.isEmpty()){
-                    names.add(name);
-                    adapter.notifyDataSetChanged();
-                    input_name.setText("");
-                }
-            }
-        });
     }
 
 
@@ -71,23 +35,5 @@ public class MainActivity extends Activity{
         return super.onOptionsItemSelected(item);
     }
 
-    class MyItemClickListener implements AdapterView.OnItemClickListener{
-        @Override
-        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-            String name = adapter.getItem(i);
-            if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT){
-                Intent action = new Intent(getApplicationContext(), NameDetailActivity.class);
-                action.putExtra(NAME_TAG, name);
-                startActivity(action);
-            }
-            else{
-                // fragment in the same activity in landscape
-                NameDetailFragment frag = (NameDetailFragment)
-                        getFragmentManager().findFragmentById(R.id.detail_fragment);
-                frag.setName(name);
-            }
-
-        }
-    }
 
 }
